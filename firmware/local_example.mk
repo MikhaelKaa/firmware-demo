69fe20cc351fee132625ae90778d85a0ee84d85f
MikhaelKaa <unionfalse@gmail.com>
2026-06-24 03:21:19 +0300

    Обновить Makefile под новую структуру с submodule firmware-core
    - Заменить include mcu/$(MCU_PATH)/device.mk на core/device.mk
    - Убрать из Makefile источники kernel (перенесены в device.mk submodule)
    - Исправить пути app/* на cli/* для всех CLI-модулей
    - Обновить micros include: -Iapp/micros -> -Icore/lib/micros
    - Обновить setup-vscode: mcu/$(MCU_PATH)/.vscode -> core/.vscode
    - Обновить OpenOCD пути: mcu/common/openocd/* -> core/vendor/openocd/*
    - Обновить local_example.mk аналогично

--- firmware/local_example.mk
+++ firmware/local_example.mk
@@ -1,3 +1,3 @@
 #  rename local_example.mk to local.mk
-# OPENOCD_TRANSPORT_CFG ?= ./firmware/mcu/common/openocd/cmsis-dap.cfg
-OPENOCD_TRANSPORT_CFG ?= ./firmware/mcu/common/openocd/stlink.cfg
+# OPENOCD_TRANSPORT_CFG ?= core/vendor/openocd/cmsis-dap.cfg
+OPENOCD_TRANSPORT_CFG ?= core/vendor/openocd/stlink.cfg
